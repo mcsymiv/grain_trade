c9db9753f5e56d05bb7c2d3c2733169169355f45
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-10-05 10:24:01 +0300

    Add FooterPage POM

--- GrainTrade/GrainTrade/PageObjects/LeftMenuPage/LeftMenuPage.cs
+++ GrainTrade/GrainTrade/PageObjects/LeftMenuPage/LeftMenuPage.cs
@@ -15,29 +15,29 @@ namespace GrainTrade.PageObjects.LeftMenuPage
             this._driver = driver;
         }
 
-        By Logo = By.CssSelector("#app > div.slide-menu > div > div.head > div > div.image > a > img");
-        By Main = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.active > a");
-        By Exchange = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(2) > a");
-        By Index = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(3) > a");
-        By Users = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc > a");
-        By Traiders = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(1) > a");
-        By Manufacturer = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(2) > a");
-        By Elevators = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(3) > a");
-        By FreightForwarders = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(4) > a");
-        By Gates = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(5) > a");
-        By Terminals = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(6) > a");
-        By Holdings = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(7) > a");
-        By Processors = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(8) > a");
-        By News = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(5) > a");
-        By GrainPrices = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(6) > a");
-        By Statistics = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(7) > a");
-        By Map = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(8) > a");
-        By AboutUs = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about > a");
-        By Contacts = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(1) > a");
-        By Partners = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(2) > a");
-        By Cooperation = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(3) > a");
-        By Advertising = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(4) > a");
-        By SiteMap = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(5) > a");
+        By _logo = By.CssSelector("#app > div.slide-menu > div > div.head > div > div.image > a > img");
+        By _main = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.active > a");
+        By _exchange = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(2) > a");
+        By _index = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(3) > a");
+        By _users = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc > a");
+        By _traiders = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(1) > a");
+        By _manufactures = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(2) > a");
+        By _elevators = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(3) > a");
+        By _freightForwarders = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(4) > a");
+        By G_gtes = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(5) > a");
+        By _terminals = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(6) > a");
+        By _holdings = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(7) > a");
+        By _processors = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(8) > a");
+        By _news = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(5) > a");
+        By _grainPrices = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(6) > a");
+        By _statistics = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(7) > a");
+        By _map = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(8) > a");
+        By _aboutUs = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about > a");
+        By _contacts = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(1) > a");
+        By _partners = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(2) > a");
+        By _cooperation = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(3) > a");
+        By _advertising = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(4) > a");
+        By _siteMap = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(5) > a");
 
 
         public LeftMenu ClickTheButton(By locator)
