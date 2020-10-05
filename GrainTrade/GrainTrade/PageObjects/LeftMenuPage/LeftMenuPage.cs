496a6c7c64247b4da62100550fcdbc3b074235a6
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-10-05 19:38:09 +0300

    Add links tests to the MenuTest class

--- GrainTrade/GrainTrade/PageObjects/LeftMenuPage/LeftMenuPage.cs
+++ GrainTrade/GrainTrade/PageObjects/LeftMenuPage/LeftMenuPage.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,42 +9,54 @@ using System.Threading.Tasks;
 
 namespace GrainTrade.PageObjects.LeftMenuPage
 {
-    public class LeftMenu
+    public class LeftMenuPage
     {
         IWebDriver _driver;
-        public LeftMenu(IWebDriver driver)
+        public LeftMenuPage(IWebDriver driver)
         {
             this._driver = driver;
         }
 
-        By _logo = By.CssSelector("#app > div.slide-menu > div > div.head > div > div.image > a > img");
-        By _main = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.active > a");
-        By _exchange = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(2) > a");
-        By _index = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(3) > a");
-        By _users = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc > a");
-        By _traiders = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(1) > a");
-        By _manufactures = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(2) > a");
-        By _elevators = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(3) > a");
-        By _freightForwarders = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(4) > a");
-        By G_gtes = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(5) > a");
-        By _terminals = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(6) > a");
-        By _holdings = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(7) > a");
-        By _processors = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.users.desc.open > div > div:nth-child(8) > a");
-        By _news = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(5) > a");
-        By _grainPrices = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(6) > a");
-        By _statistics = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(7) > a");
-        By _map = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div:nth-child(8) > a");
-        By _aboutUs = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about > a");
-        By _contacts = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(1) > a");
-        By _partners = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(2) > a");
-        By _cooperation = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(3) > a");
-        By _advertising = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(4) > a");
-        By _siteMap = By.CssSelector("#app > div.slide-menu > div > div.body.ps-container.ps-theme-default.ps-active-y > div.link-list > div > div.item.about.open > div > div:nth-child(5) > a");
+        public By MenuButton = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.without-padding-top div.header.transparent div.slide-menu_btn");
+        public By TitleMain = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[1]/a/div[2]/span");
 
+        public By Exchange = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[2]");
+        public By ExchangeTitle = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.without-padding-top div.layout-wrap div.content-wrap.container div.page-content div.page-head div.title-wrap h1.h2-title");
+        public By News = By.CssSelector("html body.no-scroll noindex div#app.page-wrap div.slide-menu div.slide-menu-wrap div.body.ps-container.ps-theme-default div.link-list div.items-wrap div.item.active a");
+        public By NewsTitle = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.default div.layout-wrap div.content-wrap.container div.page-content div.page-head h1.h2-title");
+        public By Prices = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[5]");
+        public By PriceTitle = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.default div.layout-wrap div.content-wrap.container div.page-content div.page-head h1.h2-title");
+        public By Stats = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[6]");
+        public By StatsTitle = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[2]");
+        public By Map = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[7]");
+        public By MapTitle = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.default div.layout-wrap div.content-wrap.container div.page-content div.page-head h1.h2-title");
 
-        public LeftMenu ClickTheButton(By locator)
+        public By Traiders = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[3]/div/div[1]");
+        public By TraidersPageTitle = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.default div.layout-wrap div.content-wrap.container div.page-content div.page-head div.page-head-desktop div.title-wrap h2.h2-title");
+        public By Manufacturers = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[4]/div/div[2]");
+        public By ManufacturersPageTitle = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.default div.layout-wrap div.content-wrap.container div.page-content div.page-head div.page-head-desktop div.title-wrap h2.h2-title");
+        public By Elevators = By.XPath("/html/body/noindex/div/div[2]/div/div[2]/div[1]/div/div[4]/div/div[3]");
+        public By ElevatorsPageTitle = By.CssSelector("html body noindex div#app.page-wrap div.page-layout.default div.layout-wrap div.content-wrap.container div.page-content div.page-head div.page-head-desktop div.title-wrap h2.h2-title");
+
+        public LeftMenuPage MenuLinkClick(By locator)
         {
-            _driver.FindElement(locator).Click();
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(_driver.FindElement(locator)).Click().Perform();
+            return this;
+        }
+        public LeftMenuPage MenuButtonOpen()
+        {
+            _driver.FindElement(MenuButton).Click();
+            return this;
+        }
+        public string GetElementTitle(By locator)
+        {
+            return _driver.FindElement(locator).Text;
+        }
+        public LeftMenuPage ScrollToView(By locator)
+        {
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(_driver.FindElement(locator)).Perform();
             return this;
         }
     }
--- GrainTrade/GrainTrade/Test/MenuTest/MenuTest.cs
+++ GrainTrade/GrainTrade/Test/MenuTest/MenuTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using GrainTrade.PageObjects.LeftMenuPage;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +14,91 @@ namespace GrainTrade.Test.MenuTest
     [TestFixture]
     public class MenuTest
     {
+        IWebDriver chrome;
+        WebDriverWait wait;
+        LeftMenuPage menuPage;
+        ConfigTestFlow config;
 
+
+        [SetUp]
+        public void InitTest()
+        {
+            chrome = new ChromeDriver(@"C:\Users\mcsymiv\Desktop\git\chromedriver_win32");
+            wait = new WebDriverWait(chrome, TimeSpan.FromSeconds(10));
+            menuPage = new LeftMenuPage(chrome);
+            config = new ConfigTestFlow(chrome);
+
+            chrome.Navigate().GoToUrl("https://dev.graintrade.com.ua");
+            config.Refresh();
+            chrome.Manage().Window.Maximize();
+            chrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
+        [TearDown]
+        public void CloseChromeWindow()
+        {
+            chrome.Quit();
+        }
+        [TestCase(1, "Біржа зерна Online")]
+        [TestCase(2, "Новини")]
+        [TestCase(3, "Ціни на зерно")]
+        [TestCase(4, "Статистика")]
+        [TestCase(5, "Карта")]
+        [TestCase(6, "Трейдери")]
+        [TestCase(7, "Виробники")]
+        [TestCase(8, "Елеватори")]
+        public void LeftMainMenuLinksCheck(int link, string expectedTitle)
+        {
+            String actualTitle = string.Empty;
+            menuPage.MenuButtonOpen();
+            switch (link)
+            {
+                case 1:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Exchange)
+                        .GetElementTitle(menuPage.ExchangeTitle);
+                    break;
+                case 2:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.News)
+                        .GetElementTitle(menuPage.NewsTitle);
+                    break;
+                case 3:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Prices)
+                        .GetElementTitle(menuPage.PriceTitle);
+                    break;
+                case 4:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Stats)
+                        .GetElementTitle(menuPage.StatsTitle);
+                    break;
+                case 5:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Map)
+                        .GetElementTitle(menuPage.MapTitle);
+                    break;
+                case 6:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Traiders)
+                        .GetElementTitle(menuPage.TraidersPageTitle);
+                    break;
+                case 7:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Manufacturers)
+                        .GetElementTitle(menuPage.ManufacturersPageTitle);
+                    break;
+                case 8:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Manufacturers)
+                        .GetElementTitle(menuPage.ManufacturersPageTitle);
+                    break;
+                case 9:
+                    actualTitle = menuPage
+                        .MenuLinkClick(menuPage.Elevators)
+                        .GetElementTitle(menuPage.ElevatorsPageTitle);
+                    break;
+            }
+            Assert.AreEqual(expectedTitle, actualTitle);
+        }
     }
 }
